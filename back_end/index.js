f7a229974b4e123ddfc1d7904c20ea4ec1f4b35d
Milad Nowrozi <milad927649@gmail.com>
2025-02-07 00:45:38 +0330

    repair cors origin config from env

--- back_end/index.js
+++ back_end/index.js
@@ -14,7 +14,7 @@ import { AuthToken } from "./controllers/CtrlAuth.js";
 const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: process.env.CORS_ORIGIN.split(","), credentials: true }));
 //
 // const __dirname = path.join(path.dirname(fileURLToPath(import.meta.url)));
 
