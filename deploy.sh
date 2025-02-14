cb2ffe783c3608f70603747581eaf338dbaecdd7
Milad Nowrozi <milad927649@gmail.com>
2025-02-14 15:23:22 +0330

    add docker compose -f docker-compose.develop.yml up -d backend

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ git pull origin master
 docker compose -f docker-compose.develop.yml build backend
 docker compose -f docker-compose.develop.yml build frontend
 
+docker compose -f docker-compose.develop.yml up -d backend
 docker compose -f docker-compose.develop.yml up -d frontend
