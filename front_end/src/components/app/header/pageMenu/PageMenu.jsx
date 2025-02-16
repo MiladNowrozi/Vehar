a24d38f9072d32093236fd73ab057baacd65bf29
Milad Nowrozi <milad927649@gmail.com>
2025-02-16 20:08:58 +0330

    fix page manu v-3

--- front_end/src/components/app/header/pageMenu/PageMenu.jsx
+++ front_end/src/components/app/header/pageMenu/PageMenu.jsx
@@ -65,10 +65,6 @@ export const PageMenu = () => {
 									<a href="#">مشارکت در خبرنگاری</a>
 									<a href="#">آرشیو</a>
 								</li>
-								<li></li>
-								<li></li>
-								<li></li>
-								<li></li>
 							</ul>
 						</div>
 						<div className="SocialNetworks">
