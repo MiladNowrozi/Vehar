0c29272ea8bc490dd9c82ed93c0b927fae06ac7b
Milad Nowrozi <milad927649@gmail.com>
2025-02-18 22:29:42 +0330

    change deploy.sh file to delete other images

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+# cd Vehar
+# git pull origin master
+# docker compose -f docker-compose.develop.yml build backend
+# docker compose -f docker-compose.develop.yml build frontend
+
+# docker compose -f docker-compose.develop.yml up -d backend
+# docker compose -f docker-compose.develop.yml up -d frontend
+
+# ___________________________________________________________
+
 cd Vehar
+
 git pull origin master
+
+docker compose -f docker-compose.develop.yml down
+
+docker images -q | grep -v $(docker images -q f2ad9f23df82) | xargs -r docker rmi
+
 docker compose -f docker-compose.develop.yml build backend
-docker compose -f docker-compose.develop.yml build frontend
 
 docker compose -f docker-compose.develop.yml up -d backend
-docker compose -f docker-compose.develop.yml up -d frontend
