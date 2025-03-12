43b28a9887fea6319409aa4920de2d28d1849f51
Milad Nowrozi <milad927649@gmail.com>
2025-03-12 05:30:01 +0330

    modify deploy

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ git pull origin master
 
 docker compose -f docker-compose.develop.yml down
 
-docker images --format "{{.ID}}" | grep -v f05d6e1018a8 | xargs -r docker rmi -f
+docker images --format "{{.ID}}" | grep -v f2ad9f23df82 | xargs -r docker rmi -f
 
 docker compose -f docker-compose.develop.yml build backend
 docker compose -f docker-compose.develop.yml build frontend
