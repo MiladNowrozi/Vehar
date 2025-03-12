d1867c4fa54000266ebd991203ae78c4bce19650
Milad Nowrozi <milad927649@gmail.com>
2025-03-13 02:23:52 +0330

    for test

--- back_end/controllers/CtrlDownload.js
+++ back_end/controllers/CtrlDownload.js
@@ -15,7 +15,7 @@ export default class DownloadControllers {
 		if (checkFile === "png" || checkFile === "jpeg" || checkFile === "gif" || checkFile === "jpg") {
 			const imagePath = __dirname + "/uploads/news" + req.query.name;
 			if (!fs.existsSync(imagePath)) {
-				return res.status(404).json({ error: "Image not found 5555" });
+				return res.status(404).json({ error: "Image not found 1122" });
 			}
 			const contentType = imagePath.endsWith(".png")
 				? "image/png"
