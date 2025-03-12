93860a89e36e61e2d05124123e0b2abe7a0cdf0d
Milad Nowrozi <milad927649@gmail.com>
2025-03-12 05:56:08 +0330

    add --build

--- deploy.sh
+++ deploy.sh
@@ -20,5 +20,5 @@ docker images --format "{{.ID}}" | grep -v f2ad9f23df82 | xargs -r docker rmi -f
 docker compose -f docker-compose.develop.yml build backend
 docker compose -f docker-compose.develop.yml build frontend
 
-docker compose -f docker-compose.develop.yml up -d
+docker compose -f docker-compose.develop.yml up --build -d
 # docker compose -f docker-compose.develop.yml up -d frontend
