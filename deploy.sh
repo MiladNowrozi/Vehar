0394c113af3e6a49aa97fd17f71eff82cd84c93d
Milad Nowrozi <milad927649@gmail.com>
2025-03-12 12:34:03 +0330

    chang keep mysql by name by new  method

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,8 @@ git pull origin master
 
 docker compose -f docker-compose.develop.yml down
 
-docker rmi -f $(docker images -q)
+# docker rmi -f $(docker images -q)
+docker images --format "{{.Repository}}:{{.Tag}}" | grep -v "mysql:8.0.28" | xargs -r docker rmi -f
 
 docker compose -f docker-compose.develop.yml build backend
 docker compose -f docker-compose.develop.yml build frontend
