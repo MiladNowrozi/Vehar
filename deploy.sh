bc8b8cbccb44710ba6e515cf2e163beafdaf9b2f
Milad Nowrozi <milad927649@gmail.com>
2025-03-21 12:36:46 +0330

    fix a bug

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,9 @@ git pull origin master
 pm2 delete 0
 rm -rf ./back_end/node_modules
 npm --prefix ./back_end/ i
-cd ./back_end && pm2 start index.js && cd ..
+cd ./back_end
+pm2 start index.js
+cd ..
 
 npm --prefix ./front_end i
 npm --prefix ./front_end run build
