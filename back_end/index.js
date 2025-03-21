c8e741b417e9fefd8873611a9c3360cf663d0b80
Milad Nowrozi <milad927649@gmail.com>
2025-03-21 18:24:28 +0330

    fix cors urls

--- back_end/index.js
+++ back_end/index.js
@@ -14,8 +14,9 @@ import { AuthToken } from "./controllers/CtrlAuth.js";
 const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(cors({ origin: ["http://localhost:3000", "http://locklhost:5000", "http://87.107.105.139", "http://192.168.0.2:3000"], credentials: true }));
+app.use(cors({ origin: process.env.URLS.split(","), credentials: true }));
 // const __dirname = path.join(path.dirname(fileURLToPath(import.meta.url)));
+console.log(process.env.URLS.split(","));
 
 // await db.sync({ alter: true });
 
