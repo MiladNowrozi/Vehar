5eddaa38954671f721d49423501d31769df9011c
Milad Nowrozi <milad927649@gmail.com>
2025-03-22 16:02:40 +0330

    back test show bug

--- back_end/index.js
+++ back_end/index.js
@@ -14,7 +14,7 @@ import { AuthToken } from "./controllers/CtrlAuth.js";
 const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://87.107.105.139", credentials: true }));
+app.use(cors({ origin: process.env.URLS.split(","), credentials: true }));
 // const __dirname = path.join(path.dirname(fileURLToPath(import.meta.url)));
 
 // await db.sync({ alter: true });
